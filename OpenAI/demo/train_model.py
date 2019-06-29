d5a09a96771af42222d3536bcf4d122af09a5c21
chris19980820 <43194816+chris19980820@users.noreply.github.com>
2019-06-30 01:11:35 +0800

    现在卷积神经网络模型可以正常进行训练了

--- OpenAI/demo/train_model.py
+++ OpenAI/demo/train_model.py
@@ -1,6 +1,9 @@
 # Developed by Chris Liu
 import numpy as np
 from OpenAI.demo.Cnn_model import alexnet
+import warnings
+
+warnings.filterwarnings("ignore")
 
 WIDTH = 160
 HEIGHT = 90
@@ -13,16 +16,15 @@ model = alexnet(WIDTH, HEIGHT, LR)
 hm_data = 22
 for i in range(EPOCHS):
     for i in range(1, hm_data + 1):
-        train_data = np.load('training_data-final.npy', allow_pickle=True)
+        train_data = np.load('training_data-1.npy', allow_pickle=True)
+        train = train_data[:184]
+        test = train_data[184:]
 
-        train = train_data[:-100]
-        test = train_data[-100:]
+        X = np.array(([i[0] for i in train])).reshape([-1, WIDTH, HEIGHT, 1])
+        Y = [i[1] for i in train]
 
-        X = np.array([i[0] for i in train]).reshape(-1, WIDTH, HEIGHT, 1)
-        Y = np.array([i[1] for i in train])
-
-        test_x = np.array([i[0] for i in test]).reshape(-1, WIDTH, HEIGHT, 1)
-        test_y = np.array([i[1] for i in test])
+        test_x = np.array(([i[0] for i in train])).reshape([-1, WIDTH, HEIGHT, 1])
+        test_y = [i[1] for i in test]
 
         model.fit({'input': X}, {'targets': Y}, n_epoch=1, validation_set=({'input': test_x}, {'targets': test_y}),
                   snapshot_step=500, show_metric=True, run_id=MODEL_NAME)
